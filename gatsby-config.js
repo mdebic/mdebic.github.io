55d48ae36da2fd554bdddc15515560047305365a
Manuel Debic <manudebic@googlemail.com>
2025-07-17 02:56:09 +0200

    Test theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/mdebic.github.io",
   siteMetadata: {
     title: `Holo - Gatsby Starter`,
     description: `Holo is a visually striking and highly customizable open source theme built on the powerful Gatsby framework and integrated with the versatile Decap CMS`,
